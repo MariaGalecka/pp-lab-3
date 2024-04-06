2cfc8709659584f93126b19e898a8a68d68b5e45
MariaGalecka <galecm5715195_aehit@students.vizja.pl>
2024-04-06 19:45:35 +0200

    Zadanie 1 klasa Point

---
+++ Main.java
@@ -0,0 +1,8 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(5, 15);
+        System.out.println("x:" + point.getX() + "y:" + point.getY());
+    }
+}
